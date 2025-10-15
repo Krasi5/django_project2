88b8d0581261fba8ebcc2873f75252b694ee2b1f
Krasi5 <krasi_todorova@yahoo.com>
2025-10-15 04:39:21 +0100

    allowed hosts updated

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9!oh@atl=i7swx&60+=vgzou-om)*!wb0ieb+r1a4#%_k+twv4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com',]
 
 
 # Application definition
